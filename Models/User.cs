3290f606f2bc8d588ef2793b740db0d51f2357ea
Sigurd Kvamme <sigurdkvamme@live.no>
2022-03-29 18:11:11 +0200

    added user model class

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+namespace usersApi.Models;
+
+public class User
+{
+  [Key]
+  public int Id { get; set; }
+  public string? Name { get; set; }
+  public string? Email { get; set; }
+  public int Age { get; set; }
+  public string? Address { get; set; }
+  public bool Admin { get; set; }
+}

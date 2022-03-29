195d7a78d8b3a2a920f288644fd097821e3af521
Sigurd Kvamme <sigurdkvamme@live.no>
2022-03-29 18:17:49 +0200

    added db context class

---
+++ Models/UsersAPIContext.cs
@@ -0,0 +1,10 @@
+#nullable disable
+using Microsoft.EntityFrameworkCore;
+namespace usersApi.Models;
+
+public class UsersAPIContext : DbContext
+{
+  public UsersAPIContext(DbContextOptions<UsersAPIContext> options) : base(options) { }
+
+  public DbSet<usersApi.Models.User> User { get; set; }
+}

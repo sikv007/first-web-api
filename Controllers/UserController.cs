9dcd13e9b69276efd6f0b00ec530c326ab8d631d
Sigurd Kvamme <sigurdkvamme@live.no>
2022-03-29 18:23:25 +0200

    added controller class

---
+++ Controllers/UserController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using usersApi.Models;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+  
+}

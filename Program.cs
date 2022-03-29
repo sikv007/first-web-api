f5e52c17d7c6144bc3785122013a317ef3819154
Sigurd Kvamme <sigurdkvamme@live.no>
2022-03-29 18:28:18 +0200

    added CORS

--- Program.cs
+++ Program.cs
@@ -9,11 +9,22 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<UsersAPIContext>(options => options.UseSqlite("Data source  = Users.db"));
 
+builder.Services.AddCors(
+  options => { options.AddPolicy("Allow",
+  policies => policies
+  .AllowAnyOrigin()
+  .AllowAnyHeader()
+  .AllowAnyMethod());
+  }
+);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors("Allow");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
